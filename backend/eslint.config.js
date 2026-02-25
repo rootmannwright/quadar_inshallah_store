b1ff3a193594cd0734374b17750a02110076a929
Lucas M. Nascimento <rootmannwright@gmail.com>
2026-02-25 07:34:47 -0300

    feat: Reinstalação dos packages do lint

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,51 +1,7 @@
-// eslint.config.js
-// translated by RootmannWright
 import js from "@eslint/js";
 import globals from "globals";
-import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  {
-    files: ["**/*.{js,mjs,cjs,jsx}"],
-    
-    // parserOptions é aqui
-    languageOptions: {
-      parserOptions: {
-        ecmaVersion: 2026, // versão do JS
-        sourceType: "module", // usar import/export
-      },
-      globals: globals.node, // variáveis globais do node
-    },
-
-    env: {
-      browser: true,
-      node: true,
-      es2026: true,
-    },
-
-    plugins: {
-      js,
-    },
-
-    extends: [
-      "js/recommended",             // regras básicas do @eslint/js
-      pluginReact.configs.flat.recommended, // regras recomendadas do react
-    ],
-
-    settings: {
-      react: {
-        version: "detect",
-      },
-    },
-
-    rules: {
-      // Aqui você coloca as regras que quer customizar
-      "no-console": "warn",       // console.log vira warning
-      "quotes": ["error", "double"], // força aspas duplas
-      "curly": "error",           // sempre usar {}
-      "consistent-return": "error", // arrow async precisa sempre retornar
-      "semi": ["error", "always"], // ponto e vírgula obrigatório
-    },
-  },
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
 ]);
