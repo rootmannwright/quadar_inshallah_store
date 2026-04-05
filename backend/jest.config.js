8d38503aff60602142ca695b3b932261d5232987
مختار <rootmannwright@gmail.com>
2026-04-04 23:40:21 -0300

    feat(jest.config.js): Implementação de atualização de informações para funcionar o ci.yml

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -3,12 +3,11 @@ export default {
   transform: {
     "^.+\\.jsx?$": ["babel-jest", { rootMode: "upward" }]
   },
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
   testTimeout: 15000,
   collectCoverage: true,
   coverageDirectory: "coverage",
   coverageReporters: ["json-summary", "lcov", "text", "clover"],
-  extensionsToTreatAsEsm: [".js"],
-  moduleNameMapper: {
-    "^(\\.{1,2}/.*)\\.js$": "$1",
-  },
 };
